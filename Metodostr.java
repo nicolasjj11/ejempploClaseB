887c714f3466d51f1676d4125f24d32dc2816978
nicolasjj11 <nicolasjaga700@gmail.com>
2024-10-24 20:10:35 -0500

    docs(se agrego un documento . java): se agregaron ejemplos de funciones

---
+++ Metodostr.java
@@ -0,0 +1,25 @@
+public class Metodostr {
+    public static void main(String[] args) {
+        String cadena1= "curso de ";
+        String cadena2 = " java ";
+        cadena1= cadena1 + cadena2 ;
+        System.out.println(cadena1);
+
+        // ahora para comparar cadenas
+
+    String cadenaN = " programacion ";
+     String cadenaj = " programacion ";
+         System.out.println(cadenaN.equals(cadenaj));
+
+
+         // una funcion para ver la cadena en minuscula 
+
+         String cadenak = "ProGrAMACION " ;
+         System.out.println(cadenak.toLowerCase());
+
+         // una funcion para ver la cadena en mayuscula 
+
+         String cadenaw = "programacion " ;
+         System.out.println(cadenaw.toUpperCase());
+    }
+}
